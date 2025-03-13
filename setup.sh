68ea2d6031581bf45982ef8d478bd97faf42ded1
noamgoldberg <noamgoldberg2@gmail.com>
2025-03-13 04:59:02 -0400

    s

--- setup.sh
+++ setup.sh
@@ -8,3 +8,7 @@ sudo apt-get install -y wget curl unzip
 # Install required libraries for Chromium
 sudo apt-get install -y libx11-dev libx11-xcb1 libxcomposite1 libxdamage1 libxi6 libgdk-pixbuf2.0-0 libnss3 libxtst6 libatk-bridge2.0-0 libgtk-3-0 libdbus-1-3
 sudo apt-get install -y libgbm1 libasound2 libxrandr2 libu2f-udev
+sudo apt-get install -y libglib2.0-0=2.50.3-2 \
+    libnss3=2:3.26.2-1.1+deb9u1 \
+    libgconf-2-4=3.2.6-4+b1 \
+    libfontconfig1=2.11.0-6.7+b1
